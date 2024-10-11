d856d3574b3aa6e0a6afa2854204d184b95f2649
zen <amriikhda207@gmail.com>
2024-10-11 17:39:58 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { ButtonDefault } from "../components/Button"
+
+function Home() {
+  return (
+    <div className="container mt-5 ">
+      <ButtonDefault />
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container : {
+        padding : "16px",
+        center : true,
+      }
+    },
+  },
+  plugins: [],
+}
+
