e5b30e71ce13aadbd59a318ae9750d1ee2250fc6
awiipp <sayidariipp@gmail.com>
2024-10-12 10:29:21 +0700

    test page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AOS from "aos"; 
-import "aos/dist/aos.css";
-import { useEffect } from "react"; 
+import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Test from './pages/Test';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
     AOS.init({
       duration: 1000,
     });
-  }, []); 
+  }, []);
   return (
     <>
       <Routers>
         <Routes>
-          <Route path="/" element= <Home/> />
+          <Route path="/" element=<Home /> />
+          <Route path="/test" element=<Test /> />
         </Routes>
       </Routers>
     </>
---
+++ src/pages/Test.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen">
+      <h1>LOREM IPSUM</h1>
+    </div>
+  );
+}
+
+export default Home;
